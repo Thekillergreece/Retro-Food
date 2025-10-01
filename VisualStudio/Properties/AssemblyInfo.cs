70c0fd9755f4f9844e38524893b19878b4cd7224
Thekillergreece <sakislkg@gmail.com>
2025-10-01 19:20:01 +0300

    1.5

--- VisualStudio/Properties/AssemblyInfo.cs
+++ VisualStudio/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RetroFood")]
 [assembly: AssemblyCopyright($"Created by Inarissa & Thekillergreece")]
 
-[assembly: AssemblyVersion("1.3")]
-[assembly: AssemblyFileVersion("1.3")]
-[assembly: MelonInfo(typeof(TKG.TKGMain), "RetroFood", "1.3", "Inarissa & Thekillergreece")]
+[assembly: AssemblyVersion("1.4")]
+[assembly: AssemblyFileVersion("1.4")]
+[assembly: MelonInfo(typeof(TKG.TKGMain), "RetroFood", "1.4", "Inarissa & Thekillergreece")]
 
 //This tells MelonLoader that the mod is only for The Long Dark.
 [assembly: MelonGame("Hinterland", "TheLongDark")]
